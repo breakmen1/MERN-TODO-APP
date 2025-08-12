3717a13d0eb68a61765de8ccaa654e185eeb6b2d
Akshaykumar Patil <your-akshaykumarpatil63@gmail.com>
2025-08-12 12:15:14 +0000

    update header to MyTodoApp

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -16,7 +16,7 @@ function Header() {
         <div>
             <nav className='header bg-slate-200 flex justify-between items-center'>
                 <div className="logo w-1/4 text-center">
-                    <NavLink to="/">My Todo App</NavLink>
+                    <NavLink to="/">MyTodoApp</NavLink>
                 </div>
                 <div className='flex justify-between'>
                     {
